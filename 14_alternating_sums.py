b36ed75ec464d88d965e0a054be3e172f4aa6db5
Dimitar Elenkov <dgelenkov@outlook.com>
2021-08-26 15:54:11 +0300

    Adding 14_alternating_sums.py

---
+++ 14_alternating_sums.py
@@ -0,0 +1,18 @@
+# https://app.codesignal.com/arcade/intro/level-4/cC5QuL9fqvZjXJsW9
+
+
+def alternating_sums(weights):
+    team_1 = []
+    team_2 = []
+
+    for i in range(len(weights)):
+        if i % 2 == 0:
+            team_1.append(weights[i])
+        else:
+            team_2.append(weights[i])
+
+    return [sum(team_1), sum(team_2)]
+
+
+input_user = [50, 60, 60, 45, 70]
+print(alternating_sums(input_user))
