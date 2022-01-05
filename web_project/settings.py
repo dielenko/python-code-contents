b1477c87999457466900fdea710fcb915fb37394
Dimitar Elenkov <dgelenkov@outlook.com>
2022-01-05 21:54:00 +0000

    Adding Django app called Hello  and all related MVT components

--- web_project/settings.py
+++ web_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "hello",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Defines a location where static files are collected when the collectstatic is used
+STATIC_ROOT = BASE_DIR / 'static_collected'
